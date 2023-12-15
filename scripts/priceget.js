43737507f4bc0468b316ab70dc472e9fde92d5db
ForHTMLClass2023 <151016808+ForHTMLClass2023@users.noreply.github.com>
2023-12-15 05:19:51 -0500

    Update priceget.js

--- scripts/priceget.js
+++ scripts/priceget.js
@@ -1,9 +1,8 @@
 var doge = document.getElementById("dogecoin");
 var liveprice = {
     "async": true,
-    "croosDomain": true,
+    "crossDomain": true,
     "url": "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd",
-
     "method": "GET",
     "headers": {}
 }
