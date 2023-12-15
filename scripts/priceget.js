28c9081ea6dc97728a7009ac417c208866d5c92d
ForHTMLClass2023 <151016808+ForHTMLClass2023@users.noreply.github.com>
2023-12-15 04:28:56 -0500

    Add files via upload

---
+++ scripts/priceget.js
@@ -0,0 +1,12 @@
+var doge = document.getElementById("dogecoin");
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd",
+
+    "method": "GET",
+    "headers": {}
+}
+$.ajax(liveprice).done(function (response){
+    doge.innerHTML = response.dogecoin.usd;
+});
